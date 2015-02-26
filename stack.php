872d7a06dc7f4c6f1fce5dc94b6783e011518e72
Essam Al Joubori <essam.joubori@gmail.com>
2015-02-26 10:43:08 -0500

    Initial commit

---
+++ stack.php
@@ -0,0 +1,35 @@
+<?php
+
+class Stack {
+   
+    public $storage;
+    public $size;
+
+
+	public function __construct($storage = array(), $size = 0) {
+		$this->size = $size;
+		$this->storage = $storage;
+	}
+
+
+	public function push($item){
+		array_push($this->storage, $item);
+		$this->size++;
+	}
+
+	public function pop() {
+		array_pop($this->storage);
+		$this->size--;
+	}
+}
+
+$newStack = new Stack();
+$newStack->push(10);
+$newStack->push(20);
+echo $newStack->size;
+$newStack->pop();
+
+echo $newStack->size;
+
+
+?>
